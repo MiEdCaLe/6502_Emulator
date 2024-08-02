b4c5bad59ea8bf43f29da8b0689dd2d40a6ef469
Miguel Castillo <miguel.e.castillo.leyva@gmail.com>
2024-08-01 20:50:46 -0700

    Updated up to Video 1, minute 23:29

--- 6502_Emulator.cpp
+++ 6502_Emulator.cpp
@@ -1,20 +1,100 @@
 // 6502_Emulator.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
 
 #include <iostream>
 
-int main()
+using Byte = unsigned char; //a character is 1 byte in length, so we define BYTE as a character.
+using Word = unsigned short;//a short is 2 bytes in length, so we define WORD as a short.
+
+using u32 = unsigned int; // u32 = unsigned 32 bit number
+
+struct Mem
 {
-    std::cout << "Hello World!\n";
+    static constexpr u32 MAX_MEMORY = 1024 * 64; //64 kB
+    Byte Data[MAX_MEMORY];
+
+    void Initialize()
+    {
+        for (u32 i{ 0 } ; i < MAX_MEMORY ; i++)
+        {
+            Data[i] = 0;
+        }
+    }
+
+    // Read Byte
+    Byte operator[]( u32 Address ) const
+    {
+        return Data[Address];
+    }
+};
+
+struct CPU 
+{
+ 
+    Word ProgramCounter; // 6502 boats a 16bit register as a program counter.
+    Byte StackPointer;  // 6502 has an 8 bit stack pointer.
+
+    // Registers
+    Byte A;
+    Byte X;
+    Byte Y; 
+
+    // CPU flags register (8 bits, 1 unused)
+    Byte C : 1;
+    Byte Z : 1;
+    Byte I : 1;
+    Byte D : 1;
+    Byte B : 1;
+    Byte V : 1;
+    Byte N : 1;
+
+    void Reset(Mem& Memory) // Reset processsor
+    {
+        ProgramCounter = 0xFFFC;
+        StackPointer = 0x100; // Zero Page
+        D = 0;
+        
+        // Clear flags
+        C = 0;
+        Z = 0;
+        I = 0;
+        D = 0;
+        B = 0;
+        V = 0;
+        N = 0;
+
+        //Clear registers
+        A = 0;
+        X = 0;
+        Y = 0;
+
+        Memory.Initialize();
+    }
+
+    Byte FetchByte(u32& Cycles, Mem& Memory) 
+    {
+        Byte Data = Memory[ProgramCounter];
+        ProgramCounter++;
+        Cycles--;
+        return Data;
+    }
+
+    void Execute(u32 Cycles,Mem& Memory)
+    {
+        while (Cycles > 0)
+        {
+            Byte Instruction = FetchByte(Cycles, Memory); 
+            (void)Instruction;
+        }
+    }
+};
+
+
+
+int main()
+{   
+    Mem Memory;
+    CPU p6502;
+    p6502.Reset(Memory);
+    p6502.Execute(2, Memory);
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
